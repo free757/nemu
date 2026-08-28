1e453df61ab69b1955c9e4f5a9bcde6e28a3e4fa
moslimtech <moslimtech41@gmail.com>
2026-08-29 02:07:13 +0300

    fix(security): restore equatable import in security_cubit

--- lib/features/security/presentation/cubit/security_cubit.dart
+++ lib/features/security/presentation/cubit/security_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
 import '../../../../core/utils/constants.dart';
 import '../../domain/entities/connection_status.dart';
 import '../../domain/repositories/security_repository.dart';
